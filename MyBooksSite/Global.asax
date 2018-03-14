c6fcc9381acc583bf2eb49f26eceeb63f84af125
PrizeFightLover <y.h.h.m.theunissen@gmail.com>
2018-03-14 20:58:27 +0100

    Initial commit

---
+++ MyBooksSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyBooksSite.MvcApplication" Language="C#" %>
---
+++ MyBooksSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyBooksSite.Startup))]
+namespace MyBooksSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
